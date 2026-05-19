61ef602bae16a574f33b0c406e7965feeeee4636
atelie-poppy <ateliepoppy4@gmail.com>
2026-05-19 05:31:47 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AteliePoppyShop from "./AteliePoppyShop";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AteliePoppyShop />
-  </React.StrictMode>
-);
